8709e39caf9e6cd60c7f3fceea089dd2cf3ebf5e
Camila Araújo <camilaaraujocontato@gmail.com>
2024-05-15 18:48:51 -0300

    TESTEZINHO DA PASTA HTDOCS

--- menuscript.js
+++ menuscript.js
@@ -3,3 +3,4 @@ document.querySelector('.hamburger-menu').addEventListener('click', function () 
     document.querySelector('ul').classList.toggle('show'); 
     this.classList.toggle('change');
 });
+// AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
