3861dd0514cd262f7ef2b0740c22a9968c8fca8e
Guilherme Belo <gguilhermeferreira1@gmail.com>
2024-05-18 01:35:52 -0300

    arrumando feed

--- feed.php
+++ feed.php
@@ -1,5 +1,5 @@
 <?php
-  include("config.php");
+  include("../config.php");
 ?>
 <!DOCTYPE html>
 <html lang="PT-br">
@@ -41,7 +41,7 @@
 <div class="lancamento">
   <p>PRODUTOS</p>
   <div class="destaques">
-    <?php include_once("produtoslogado.php"); ?>
+    <?php include_once("../produtos/produtoslogado.php"); ?>
   </div>
 </div>
 <footer class="text-center footer text-center">
