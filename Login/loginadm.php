9e01bafe961ca29cdce81c87da0656e543b870b3
julianodtk <juliano.dtk.eng.software@gmail.com>
2024-05-19 16:25:24 -0300

    Ajustado da Endereço da pasta login na adm

--- Login/loginadm.php
+++ Login/loginadm.php
@@ -14,7 +14,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         // Se o administrador existir, redirecione para a página de agendamento
         $row = $result->fetch_assoc();
         $admin_id = $row['id'];
-        header("Location: ../agendamento/agendamentoadmin.php?id=$admin_id");
+        header("Location: ../Agendamento/agendamentoadmin.php?id=$admin_id");
         exit();
     } else {
         // Se as credenciais estiverem incorretas, redirecione de volta para a página de login
