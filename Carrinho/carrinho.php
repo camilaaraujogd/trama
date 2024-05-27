ef55a2abaf32b300e1b7c924d23b8dcf70e8a3ab
julianodtk <juliano.dtk.eng.software@gmail.com>
2024-05-26 22:05:31 -0300

    logo carrinho

--- Carrinho/carrinho.php
+++ Carrinho/carrinho.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Meu Carrinho</title>
     <link rel="stylesheet" href="carrinhostyle.css">
+    <link rel="icon" href="../Imagens/trama_logo_small.svg">
     <script src="../javascript/headerCliente.js" defer></script>
 </head>
 <body>
