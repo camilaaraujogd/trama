0f29fc66d596a7adbafe123032a9a5c3273ca9ba
julianodtk <juliano.dtk.eng.software@gmail.com>
2024-05-26 21:28:56 -0300

    Ajustes no CSS de visualizar produtos

--- produtos/visualizar_produtos.php
+++ produtos/visualizar_produtos.php
@@ -1,3 +1,27 @@
+<!DOCTYPE html>
+<html lang="PT-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TRAMA - MODA SUSTENTÁVEL</title>
+    <link rel="stylesheet" href="..\SemLogin\novidadesstyle.css">
+    <link rel="icon" href="..\imagens\trama_logo_small.svg">
+    <script src="..\javascript\headerProduto.js" defer></script>
+    <script src="..\javascript\Footer.js" defer></script>
+</head>
+<body>
+<header>
+    <div class="logo">
+        <a href="../index.html"><img src="../Imagens/trama_logo.png"></a>
+    </div>
+    <ul>
+        <li><a class="navlink" href="../SemLogin/sobre.html">SOBRE</a></li>
+        <li><a class="navlink" href="../produtos/visualizar_produtos.php">PRODUTOS</a></li>
+        <li><a class="navlink" href="../SemLogin/sustentabilidade.html">SUSTENTABILIDADE</a></li>
+    </ul>
+</header>
+
+
 <?php
 include_once('../config.php');
 
@@ -22,7 +46,7 @@ $sql = "SELECT * FROM produtos";
 $resultado = $conexao->query($sql);
 
 if ($resultado->num_rows > 0) {
-    // Exibe os dados de cada produto
+            // Exibe os dados de cada produto
     while($row = $resultado->fetch_assoc()) {
         echo "<h1>" . $row['nome'] . "</h1>";
         echo "<h2>Marca: " . $row['marca'] . "</h2>";
@@ -30,7 +54,6 @@ if ($resultado->num_rows > 0) {
         echo "<p>Preço: R$" . number_format($row['preco'], 2, ',', '.') . "</p>";
         echo "<img src='../uploads/" . $row['imagem'] . "' width='200'><br>";
         echo "<a href='?delete_id=" . $row['id'] . "'>Excluir</a><br><br>"; 
-        
     }
 } else {
     echo "Nenhum produto encontrado.";
@@ -39,3 +62,6 @@ if ($resultado->num_rows > 0) {
 
 $conexao->close();
 ?>
+
+</body>
+</html>
