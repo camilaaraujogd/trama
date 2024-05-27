8182f8fd5c3b9879e339e6681b89af1a5e38bcdf
julianodtk <juliano.dtk.eng.software@gmail.com>
2024-05-26 21:52:22 -0300

    ajuste CSS visualizar produtos

--- produtos/visualizar_produtos.php
+++ produtos/visualizar_produtos.php
@@ -21,47 +21,53 @@
     </ul>
 </header>
 
+<div class="containerbg">
+    <div class="container">
+        <div class="title">
+            <h5>SEUS PRODUTOS</h5>
+        </div>
+            <?php
+            include_once('../config.php');
 
-<?php
-include_once('../config.php');
+            session_start();
 
-session_start();
-
-// Verifica se foi solicitada a exclusão de algum produto
-if(isset($_GET['delete_id'])) {
-    $id = $_GET['delete_id'];
+            // Verifica se foi solicitada a exclusão de algum produto
+            if(isset($_GET['delete_id'])) {
+                $id = $_GET['delete_id'];
+        
     
-   
-    $sql_delete = "DELETE FROM produtos WHERE id = $id";
-    
-    if ($conexao->query($sql_delete) === TRUE) {
-        echo "Produto excluído com sucesso.";
-    } else {
-        echo "Erro ao excluir o produto: " . $conexao->error;
-    }
-}
+                $sql_delete = "DELETE FROM produtos WHERE id = $id";
+        
+                if ($conexao->query($sql_delete) === TRUE) {
+                    echo "Produto excluído com sucesso.";
+                } else {
+                    echo "Erro ao excluir o produto: " . $conexao->error;
+                }
+            }
 
 
-$sql = "SELECT * FROM produtos";
-$resultado = $conexao->query($sql);
+            $sql = "SELECT * FROM produtos";
+            $resultado = $conexao->query($sql);
 
-if ($resultado->num_rows > 0) {
-            // Exibe os dados de cada produto
-    while($row = $resultado->fetch_assoc()) {
-        echo "<h1>" . $row['nome'] . "</h1>";
-        echo "<h2>Marca: " . $row['marca'] . "</h2>";
-        echo "<h4>Descrição: " . $row['descricao'] . "</h4>";
-        echo "<p>Preço: R$" . number_format($row['preco'], 2, ',', '.') . "</p>";
-        echo "<img src='../uploads/" . $row['imagem'] . "' width='200'><br>";
-        echo "<a href='?delete_id=" . $row['id'] . "'>Excluir</a><br><br>"; 
-    }
-} else {
-    echo "Nenhum produto encontrado.";
-}
-    echo " <a href='../produtos/colocarprodutos.html'>Voltar</a>";
+            if ($resultado->num_rows > 0) {
+                        // Exibe os dados de cada produto
+                while($row = $resultado->fetch_assoc()) {
+                    echo "<h1>" . $row['nome'] . "</h1>";
+                    echo "<h2>Marca: " . $row['marca'] . "</h2>";
+                    echo "<h4>Descrição: " . $row['descricao'] . "</h4>";
+                    echo "<p>Preço: R$" . number_format($row['preco'], 2, ',', '.') . "</p>";
+                    echo "<img src='../uploads/" . $row['imagem'] . "' width='200'><br>";
+                    echo "<a href='?delete_id=" . $row['id'] . "'>Excluir</a><br><br>"; 
+                }
+            } else {
+                echo "Nenhum produto encontrado.";
+            }
+                echo " <a href='../produtos/colocarprodutos.html'>Voltar</a>";
 
-$conexao->close();
-?>
+            $conexao->close();
+            ?>
 
+    </div>
+</div>
 </body>
 </html>
